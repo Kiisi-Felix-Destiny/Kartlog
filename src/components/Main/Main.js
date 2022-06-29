6b61ff601f1821c27d23b4a05b39aa2f7f28c948
Kiisi Felix Destiny <98293558+Kiisi-Felix-Destiny@users.noreply.github.com>
2022-06-29 14:08:41 +0100

    Done

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,21 +1,11 @@
 import React from 'react'
 
 import Fashion from './Fashion/Fashion'
-{/*import Activity from './Activity/Activity'
-import Benefits from './Benefits/Benefits'
-import Contact from './Contact/Contact'
-import Digital from './Digital/Digital'
-import GetStarted from './GetStarted/GetStarted'*/}
 
 const Main = () => {
   return (
     <div className="k-wrapper">
         <Fashion/>
-        {/*<Digital/>
-        <Benefits/>
-        <Activity/>
-        <GetStarted/>
-        <Contact/>*/}
     </div>
   )
 }
