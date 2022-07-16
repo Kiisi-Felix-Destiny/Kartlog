a9f897a53154c7f22475886421448d5d944e8dd2
Kiisi Felix Destiny <98293558+Kiisi-Felix-Destiny@users.noreply.github.com>
2022-07-16 17:02:15 +0100

    Done

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Header.css'
+import kartlog from '../../Images/Kartlogowhite.svg';
 const Header = () => {
   return (
     <header className="k-header">
-
+      <div className="header-container">
+        <div className="header-logo">
+          <img src={kartlog} alt="kartlog-log"/>
+        </div>
+        <div className="header-main">
+          <h1 className="header-main-1">Buy and sell</h1>
+          <h1 className="header-main-2">Custom-made clothing</h1>
+        </div>
+        <div className="header-sub">
+          <p>A marketplace for fashion designers and fashion lovers</p>
+        </div>
+      </div>
     </header>
   )
 }
